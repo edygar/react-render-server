a7d7022c36cf7792d403bd241bc945c9d6349f0c
Edygar de Lima Oliveira <edygardelima@gmail.com>
2015-12-18 17:29:57 -0200

    Docs

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,10 +4,9 @@ import image from './react-logo.svg';
 
 export default class App extends Component {
   render() {
-
     return (
       <div className={styles.helloMessage}>
-        Hello <img src={image} alt='React.js' /> World
+        Hello <img src={image} alt='React.js' /> World!
       </div>
     );
   }
